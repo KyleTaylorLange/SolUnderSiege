ca999f0f5dc92b82f2b02808f04d11213f8db8b6
Kyle Taylor Lange <dakatsu@gmail.com>
2021-10-12 22:57:42 -0400

    Initial creation of Sandbox game mode (no rules).

---
+++ Source/Lastim/Private/GameMode_Sandbox.cpp
@@ -0,0 +1,10 @@
+// Copyright Kyle Taylor Lange
+
+#include "Lastim.h"
+#include "GameMode_Sandbox.h"
+
+AGameMode_Sandbox::AGameMode_Sandbox(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	DisplayName = NSLOCTEXT("GameMode_Sandbox", "Sandbox", "Sandbox");
+
+}
